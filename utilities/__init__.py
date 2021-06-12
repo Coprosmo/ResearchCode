437d4e10a9837d79cd0018a63dac30bb84137e6f
Henry Z <henry@arcanum.ai>
2021-06-12 19:05:21 +1200

    Refactor code

--- utilities/__init__.py
+++
@@ -1,2 +0,0 @@
-from utilities.dataset_generation import bfs_visit, process_theorem, get_fingerprints, \
-        get_theorems, thm_to_tree, graph_to_data, make_data, merge_subexpressions, get_data_from_file
